f63d5c469b9beee814aafb7115030642bef086d4
Narendra <narendranarwade2002@gmail.com>
2023-04-25 13:26:43 +0530

    first commit

---
+++ Functons/app.js
@@ -0,0 +1,39 @@
+// Q1-
+function first() {
+    console.log("Hello Check");
+}
+first();
+
+// Q2-
+function add(a, b,){
+    a = 3;
+    b = 4;
+
+    var sum = a+b;
+    console.log("Sum of 3,4 is",sum);
+}
+add();
+
+// Q3-
+
+const arrow = ()=>{
+    console.log("Its arrow function");
+}
+    arrow();
+// Q4-
+var girl = function (){
+    var x = 20;
+    x++;
+    console.log(x)
+};
+girl();
+
+// Q5-
+var x = 21;
+girl();
+console.log(x);
+
+function girl(){
+    var x = 20;
+    console.log(x);
+};
